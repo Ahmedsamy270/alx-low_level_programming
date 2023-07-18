a67c221bccecd7ed1ea53f99993684b560e438e0
Ahmedsamy270 <ahmedsamy270@gmail.com>
2023-07-18 15:25:21 -0700

    ${var}

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point
+*Description: program that prints _putchar, followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char str[] = "_putchar";
+int ch;
+for (ch = 0; ch < 8; ch++)
+_putchar(str[ch]);
+_putchar('\n');
+return (0);
+}
