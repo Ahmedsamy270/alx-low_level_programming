fff5a06c14fc644f46eb210bd95fd33db6eb0bd3
Ahmedsamy270 <ahmedsamy270@gmail.com>
2023-09-11 13:09:21 -0700

    ${var}

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - is the function
+ * @name: is the name
+ * @age: is the age
+ * @owner: is the owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
