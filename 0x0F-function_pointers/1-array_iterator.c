028c83122f49712c6696ea236727018106a1651e
Ahmedsamy270 <ahmedsamy270@gmail.com>
2023-09-12 21:29:34 -0700

    ${var}

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - is a function to be excuted
+ * @array: is an array to be printed
+ * @size: is the size of the array
+ * @action: is the pinter to function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
