a1e9e967a6e0efe1f8ae2b6e57b73da70ca6b381
Ahmedsamy270 <ahmedsamy270@gmail.com>
2023-09-14 16:11:03 -0700

    ${var}

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,6 +11,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	char *str;
+	
 	va_list list;
 
 	for (i = 0; i < n; i++)
@@ -26,5 +27,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s%s", separator, str);
 	}
 	printf("\n");
+	
 	va_end(list);
 }
