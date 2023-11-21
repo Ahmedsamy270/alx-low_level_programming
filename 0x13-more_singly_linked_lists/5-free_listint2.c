c39fcf04eaead32daa09dce56b1bfa23f3a55799
Ahmedsamy270 <ahmedsamy270@gmail.com>
2023-11-21 20:25:27 +0200

    ${var}

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - is a function that free nodes and set head to null
+ * @head: is a pointer to head of linked list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (*head == NULL)
+		return;
+
+	else
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	*head = NULL;
+}
