81fb3a79fb07391dcc77e087d8711ce8f282df37
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-01-11 22:00:07 +0200

    ${var}

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,10 +1,21 @@
 #include "lists.h"
 
 /**
- *
- *
+ * dlistint_len - is a function that return the number of nodes
+ * @h: is a pointer to head node
+ * Return: The Number Of Counted Nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+	int count = 0;
 
+	if (h == NULL)
+		return (count);
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
