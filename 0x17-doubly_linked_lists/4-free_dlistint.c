3d06bdbff05d80f3990305825e7a782bc0e371ad
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-01-12 01:26:21 +0200

    ${var}

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,11 +1,24 @@
 #include "lists.h"
 
 /**
- *
- *
+ * free_dlistint -  is a function that frees all the node
+ * @head: is a pointer to the head node
  */
 
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *tmp;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+	}
+
+	while ((tmp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
+	}
 
 }
