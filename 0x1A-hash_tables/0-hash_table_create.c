84df28f35e298608c70f1ab5c9bd919bb8ab02e4
Ahmedsamy270 <ahmedsamy270@gmail.com>
2024-02-02 23:30:02 +0200

    ${var}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ptr->array = malloc(sizeof(hash_node_t *) * size);
 	if (ptr->array == NULL)
 	{
-		free(ptr);
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
